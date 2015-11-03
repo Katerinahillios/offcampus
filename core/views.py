330066fb6fa8216d2b465d52580cff5e5d7cd062
Katerina <katerinahillios@icloud.com>
2015-11-03 19:49:39 +0000

    Add business casual theme

--- core/views.py
+++ core/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 
 # Create your views here.
-class Home(TemplateView):
-    template_name = 'home.html'
+class Index(TemplateView):
+    template_name = 'index.html'
