6825514d040ff46cc629688afc9eaba19e380ce2
Katerina <katerinahillios@icloud.com>
2015-11-05 22:14:50 +0000

    Add place delete view

--- core/urls.py
+++ core/urls.py
@@ -8,5 +8,6 @@ urlpatterns = patterns('',
     url(r'^place/create/$', PlaceCreateView.as_view(), name='place_create'), 
     url(r'^place/$', PlaceListView.as_view(), name='place_list'),    
     url(r'^place/(?P<pk>\d+)/$', PlaceDetailView.as_view(), name='place_detail'),
-    url(r'^place/update/(?P<pk>\d+)/$', PlaceUpdateView.as_view(), name='place_update'),                   
+    url(r'^place/update/(?P<pk>\d+)/$', PlaceUpdateView.as_view(), name='place_update'),  
+    url(r'^place/delete/(?P<pk>\d+)/$', PlaceDeleteView.as_view(), name='place_delete'),                       
 )
--- core/views.py
+++ core/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView, CreateView, ListView, DetailView, UpdateView
+from django.views.generic import TemplateView, CreateView, ListView, DetailView, UpdateView, DeleteView
 from django.core.urlresolvers import reverse_lazy
 from .models import *
 
@@ -29,3 +29,8 @@ class PlaceUpdateView(UpdateView):
   model = Place
   template_name = 'place/place_form.html'
   fields = ['category', 'place', 'description']
+
+class PlaceDeleteView(DeleteView):
+  model = Place
+  template_name = 'place/place_confirm_delete.html'
+  success_url = reverse_lazy('place_list')
